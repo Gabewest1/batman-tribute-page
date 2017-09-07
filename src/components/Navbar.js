76b20222a2fdd83776214adf185cda2ba13b47a1
Gabriel West <gabe_west1@yahoo.com>
2017-09-07 00:18:57 -0500

    Added functionallity to transition the logo as the user scrolls down the page.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,8 +6,7 @@ const Nav = styled.div`
     background: black;
     grid-area: nav;
     display: flex;
-    align-items: center;
-    padding: 15px 10px;
+    padding: 15px;
     box-sizing: border-box;
     top: 0;
     width: 100%;
@@ -16,17 +15,29 @@ const Nav = styled.div`
 `
 const BatmanLogo = styled.img`
     max-height: 100%;
-    max-width: 10%;
+    max-width: 10%;   
+    position: relative;
+    left: ${({ left }) => left || "0px"}; 
+    transform: 
+        translateX(${({ progress }) => progress})
+        rotate(${({ progress }) => progress.replace("px", "deg")});
+    z-index: 1;
 `
 const Timeline = styled.ul`
     padding: 0;
     margin: 0;
+    position: absolute;
     height: 3px;
     width: 100%;
     background: silver;
     list-style: none;
     display: flex;
     justify-content: space-between;
+`
+const TimelineContainer = styled.div`
+    position: relative;
+    display: flex;
+    align-items: center;
 `
 const Bullet = styled.li`
     height: 15px;
@@ -38,19 +49,83 @@ const Bullet = styled.li`
 `
 
 class Navbar extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            startingLeftPosition: 0,
+            bulletsCenterPositions: [],
+            progress: "0px",
+        }
+    }
+    componentDidMount() {
+        console.log("STATE B4:", this.state)
+        this.getCenterOfBullets()
+        setTimeout(() => {
+            console.log("STATE AFTER:", this.state)
+            this.moveLogoToFirstBullet(this.state.bulletsCenterPositions[0])
+        }, 1)
+
+        document.addEventListener("scroll", (e) => {
+            const timelineWidth = this.timeline.getBoundingClientRect().width
+            const maxScrollAmount = document.body.scrollHeight - window.innerHeight
+            const amountScrolled = window.scrollY
+
+            const x = (amountScrolled * timelineWidth) / maxScrollAmount
+            this.setState({ progress: `${x}px` })
+        })
+    }
+    getCenterOfBullets() {
+        const bullets = Array.from(this.timeline.children)
+        console.log("BULLETS:", bullets)
+
+        const positions = bullets.map((bullet) => {
+            const { left, width } = bullet.getBoundingClientRect()
+            const style = bullet.currentStyle || window.getComputedStyle(bullet)
+            const margin = parseFloat(style.marginLeft) + parseFloat(style.marginRight)
+            const padding = parseFloat(style.paddingLeft) + parseFloat(style.paddingRight)
+            const border = parseFloat(style.borderLeftWidth) + parseFloat(style.borderRightWidth)
+
+            console.log(left, width, margin, padding, border, width + margin + padding + border)
+            const realWidth = width + margin + padding + border
+            const center = left + (realWidth / 2)
+
+            return center
+        })
+
+        this.setState({
+            bulletsCenterPositions: positions,
+        })
+    }
+    moveLogoToFirstBullet(newCenter) {
+        const { left, width } = this.logo.getBoundingClientRect()
+        const currentCenter = left + (width / 2)
+        const delta = `${newCenter - currentCenter}px`
+        console.log("Moving Logo To:", newCenter)
+        this.setState({
+            startingLeftPosition: delta,
+        })
+    }
 	render() {
+        const { startingLeftPosition, progress } = this.state
+
 		return (
 			<Nav { ...this.props }>
-				<BatmanLogo src="/images/batman-logo.png" />
-				<Timeline>
-					<Bullet />
-					<Bullet />
-					<Bullet />
-					<Bullet />
-					<Bullet />
-					<Bullet />
-					<Bullet />
-				</Timeline>
+                <TimelineContainer>
+                    <BatmanLogo
+                        innerRef={ el => this.logo = el }
+                        src="/images/batman-logo.png"
+                        left={ startingLeftPosition }
+                        progress={ progress } />
+                    <Timeline innerRef={ el => this.timeline = el }>
+                        <Bullet />
+                        <Bullet />
+                        <Bullet />
+                        <Bullet />
+                        <Bullet />
+                        <Bullet />
+                        <Bullet />
+                    </Timeline>
+                </TimelineContainer>
 			</Nav>
 		)
 	}
