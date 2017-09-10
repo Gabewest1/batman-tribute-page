684224fa1365cab0244a30d71f1e5b20011c1e41
Gabriel West <gabe_west1@yahoo.com>
2017-09-10 02:17:08 -0500

    Removed the <Header /> from the <App />. Moved its text into the <BatmanHistory />.

--- src/App.js
+++ src/App.js
@@ -5,33 +5,14 @@ import Timeline from "./components/Timeline"
 import BatmanHistory from "./components/BatmanHistory"
 import "./App.css"
 
-const Header = styled.h1`
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	font-size: 2.5em;
-	grid-area: header;
-	font-family: JusticeLeague;
-	margin: 0;
-`
 class App extends Component {
 	render() {
-		const src = window.innerWidth < 600
-			? "/videos/batman-hero-mobile.mp4"
-			: "/videos/batman-hero.mp4"
-		const headerStyles = {
-			fontFamily: "JusticeLeague",
-			gridArea: "header",
-			margin: 0,
-
-		}
 		return (
 			<div className="App">
-				{/* <Header style={{ gridArea: "header" }}>The Dark Knight</Header> */}
-				<BatmanHero style={{ gridArea: "hero" }} src={ src } />
+				<BatmanHero style={{ gridArea: "hero" }} />
 				<BatmanHistory style={{ gridArea: "history" }} />
 				<footer className="footer" style={{ gridArea: "footer" }}>
-          To learn more about <a href="#">Batman</a>
+          			To learn more about <a href="#">Batman</a>
 				</footer>
 			</div>
 		)
