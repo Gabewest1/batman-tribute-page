094eb9247fb67bf10ff3091f73d69cf6b100f9ca
Gabriel West <gabe_west1@yahoo.com>
2017-09-13 13:27:08 -0500

    Modified <BatmanHistory /> to track the scroll progress of the timeline and to animate in the <TimelineItem /> when scrolled to.

--- src/components/BatmanHistory.js
+++ src/components/BatmanHistory.js
@@ -1,6 +1,7 @@
 import React from "react"
 import styled from "styled-components"
 import Timeline from "./Timeline"
+import $ from "jquery"
 
 let TimelineItem = styled.div`
     box-sizing: border-box;
@@ -13,6 +14,12 @@ let TimelineItem = styled.div`
         "image"
         "desc" !important;
 
+    ${({ position, progress }) => {
+        position = position ? parseFloat(position.replace(/(px)|(%)/g, "")) : position
+        progress = progress ? parseFloat(progress.replace(/(px)|(%)/g, "")) : progress
+        return progress >= position ? "transform: translateX(0) !important;" : ""
+    }};
+        
     @media (max-width: 480px) {
         font-size: 8px;
     }
@@ -39,12 +46,14 @@ let Container = styled.div`
         ".     timeline .";
     
     > ${TimelineItem}:nth-child(odd) {
+        transform: translateX(-100%);
         grid-template-areas:
             "image year"
             "image desc";
     }
     
     > ${TimelineItem}:nth-child(even) {
+        transform: translateX(100%);        
         grid-template-areas:
             "year image"
             "desc image";
@@ -76,7 +85,8 @@ class BatmanHistory extends React.Component {
     constructor() {
         super()
         this.state = {
-            timelineItemsPositions: []
+            timelineItemsPositions: [],
+            progress: "0px"
         }
 
         this.timelineItems = []
@@ -84,17 +94,43 @@ class BatmanHistory extends React.Component {
     componentDidMount() {
         setTimeout(() => {
             console.log(this.timelineItems)
-            const positions = this.timelineItems.map(item => `top: ${item.offsetTop}px`)
+            const positions = this.timelineItems.map(item => `${item.item.offsetTop}px`)
             console.log("TOP VALUES:", positions)
             this.setState({ timelineItemsPositions: positions })
-
         }, 1)
+        document.addEventListener("scroll", (e) => {
+            this.setState({ progress: `${this.getScrollProgress()}px` })
+        })
+
+        this.setState({ progress: `${this.getScrollProgress()}px` })
+    }
+    getScrollProgress() {
+        const startingPosition = $(this.timeline).position().top
+        const timelineLength = this.timeline.getBoundingClientRect().height
+        const maxScrollAmount = document.body.scrollHeight - window.innerHeight - startingPosition
+        const amountScrolled = Math.max(0, (window.scrollY - startingPosition))
+
+        return (amountScrolled * timelineLength) / maxScrollAmount
+    }
+    addTimelineItem({ id, item }) {
+        if (this.timelineItems.filter(el => el.id === id).length === 0) {
+            this.timelineItems.push({ id, item })
+        }
     }
     render() {
+        const { progress, timelineItemsPositions } = this.state
+        console.log(progress, timelineItemsPositions)
         return (
             <Container>
-                <Timeline style={{ gridArea: "timeline" }} bullets={ this.state.timelineItemsPositions } />
-                <TimelineItem innerRef={ (el) => this.timelineItems.push(el) }style={{ gridArea: "item1" }}>
+                <Timeline
+                    style={{ gridArea: "timeline" }}
+                    bullets={ this.state.timelineItemsPositions }
+                    innerRef={ (el) => this.timeline = el } />
+                <TimelineItem 
+                    innerRef={ (el) => this.addTimelineItem({ id: 0, item: el }) }
+                    style={{ gridArea: "item1" }}
+                    progress={ progress }
+                    position={ timelineItemsPositions[0] }>
                     <Image src="/images/batman1.jpg" />
                     <Year>1920</Year>
                     <Description>
@@ -106,7 +142,11 @@ class BatmanHistory extends React.Component {
                     </Description>
                 </TimelineItem>
 
-                <TimelineItem innerRef={ (el) => this.timelineItems.push(el) }style={{ gridArea: "item2" }}>
+                <TimelineItem
+                    innerRef={ (el) => this.addTimelineItem({ id: 1, item: el }) }
+                    style={{ gridArea: "item2" }}
+                    progress={ progress }
+                    position={ timelineItemsPositions[1] }>
                     <Image src="/images/batman1.jpg" />
                     <Year>1920</Year>
                     <Description>
